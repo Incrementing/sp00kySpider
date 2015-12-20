3a05e262bba2c75ca91a23d8a5e337e361c8a76e
Gian <Incr3m3nting@gmail.com>
2015-12-20 13:49:25 +0000

    Added checking/creating log file

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 import os
+import time
 
 class Config():
+    @classmethod
+    def name(self):
+        return "sp00kySpider"
 
-	def __init__(self, output_path, output_name):
-		print(output_path)
-		print(output_name)
+    def __init__(self, output_path, output_name):
+        print("Checking if output file exists...")
+        if (not os.path.isfile(output_path + output_name)):
+            print("File doesn't exists. Making it...")
+            try:
+            	if (not os.path.exists(output_path)):
+            		os.makedirs(output_path)
+                file_ = open(output_path + output_name, 'w')
+                file_.write("Log file created by " + self.name() + " on " + time.strftime("%d/%m/%Y @ %I:%M:%S") + ".")
+                print("File created (\""+output_path+output_name+"\").")
+            except OSError:
+                print("\nError: Failed to create file (Errno 13: Permission denied)!")
+        else:
+            print("File exists.")
 
 def main():
     config = Config(output_path="/home/ad/Desktop/", output_name="crawl_log.txt")
